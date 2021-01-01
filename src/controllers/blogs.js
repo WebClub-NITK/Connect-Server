1686ceb3b6d10ca246f36750ebaae60688199e0b
Adarsh Naidu <naidu.adarsh2511@gmail.com>
2021-01-01 11:32:09 +0530

    node,express,mongodb project setup + blogs module boilerplate

---
+++ src/controllers/blogs.js
@@ -0,0 +1,28 @@
+const blogsRouter = require('express').Router()
+
+const { getAllBlogs, insertBlog } = require('../services/blogServices')
+
+blogsRouter.get('/', async(request, response) => {
+	try{
+		const blogs = getAllBlogs()
+		response.json(blogs)
+	} catch(err) {
+		console.log(err)
+		response.status(500).send('Something went wrong')
+	}
+})
+
+blogsRouter.post('/', async(request, response) => {
+	try{
+		const body = request.body
+
+		const savedBlog = await insertBlog(body)
+
+		response.status(200).json(savedBlog)
+	} catch(error) {
+		console.log(error.message)
+		response.status(400).json({ error: error.message })
+	}
+})
+
+module.exports = blogsRouter
