5b3ee780bb6c57bf9e5b4e75cce38ba27523dfbf
namanmanish <namanmanishtrivedi@gmail.com>
2021-01-02 17:41:41 +0530

    Setup basic for connecting to MySQl

---
+++ src/database/mysqldb.js
@@ -0,0 +1,35 @@
+const {PASSWORD, USER, DATABASE} = require('../utils/config')
+const mysql = require('mysql')
+
+configs = {
+    host: 'localhost',
+    user: USER,
+    password: PASSWORD,
+    database: DATABASE
+}
+const connection = mysql.createConnection(configs)
+
+const Executequery = (q,callback) => {
+    let ans = null
+    connection.connect();
+    connection.query(q, (err,res) => {
+        if(err)
+            ans = callback(null,err)
+        else   
+            ans = callback(res,null)
+    })
+    connection.end();
+    return ans
+}
+
+module.exports = Executequery
+
+
+// sample Use Case in a File
+/* 
+    const tmp = require('./database/mysqldb')
+    tmp('SELECT * FROM authors;',(res,err) => {
+        console.log(res)
+        // Your function logic
+    })
+*/
--- src/utils/config.js
+++ src/utils/config.js
@@ -2,8 +2,14 @@ require('dotenv').config()
 
 const PORT = process.env.PORT
 const MONGODB_URI = process.env.MONGODB_URI
+const USER = process.env.USER
+const PASSWORD = process.env.PASSWORD
+const DATABASE = process.env.DATABASE
 
 module.exports = {
 	MONGODB_URI,
 	PORT,
+	PASSWORD,
+	USER,
+	DATABASE
 }
