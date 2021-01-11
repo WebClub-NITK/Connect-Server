f3e9fa4c3581ea42b93d36e740d8a9e3dce097e6
Omanshu Mahawar <omanshumahawar.181co237@nitk.edu.in>
2021-01-11 22:45:58 +0530

    added resource-hub models and POST routes

--- src/controllers/resources.js
+++ src/controllers/resources.js
@@ -1,21 +1,87 @@
-const { response } = require('../app')
-
 const resourcesRouter = require('express').Router()
+require('../models/resource-module/branch')
+require('../models/resource-module/course')
+const mongoose = require('mongoose')
+const {ObjectId} = mongoose.Types.ObjectId;
+
+const Branch = mongoose.model("Branch")
+const Course = mongoose.model("Course")
 
 resourcesRouter.get('/resources', (request, response) => {
     response.send('get all resources')
 })
 
 resourcesRouter.get('/branches', (request, response) => {
-    response.send('get all branches')
+    Branch.find()
+    .then((branches) => {
+        response.json({branches: branches})
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+})
+
+resourcesRouter.post('/branches', (request, response) => {
+    const {code, name} = request.body
+    
+    if(!code || !name) {
+        return response.status(422).json({error: "please add all the fields"})
+    }
+
+    const branch = new Branch({
+        code,
+        name
+    })
+
+    branch.save()
+    .then((branch) => {
+        response.json({message: "Branch Saved Successfully"})
+    })
+    .catch((err) => {
+        console.log(err)
+    })
 })
 
 resourcesRouter.get('/courses', (request, response) => {
-    response.send('get all courses')
+    Course.find()
+    .then((courses) => {
+        response.json({courses: courses})
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+})
+
+resourcesRouter.post('/courses', (request, response) => {
+    const {code, name, branch} = request.body
+
+    if(!code || !name || !branch) {
+        return response.status(422).json({error: "please add all the fields"})
+    }
+
+    const course = new Course({
+        code,
+        name,
+        branch: ObjectId(branch)
+    })
+
+    course.save()
+    .then((course) => {
+        response.json({message: "Course Saved Successfully"})
+    })
+    .catch((err) => {
+        console.log(err)
+    })
 })
 
 resourcesRouter.get('/courses/:branch_id', (request, response) => {
-    response.send('get courses of branch '+request.params.branch_id)
+    Course.find({branch: ObjectId(request.params.branch_id)})
+    .then((courses) => {
+        response.json({courses: courses})
+    })
+    .catch((err) => {
+        console.log(err)
+    })
 })
 
 resourcesRouter.get('/resources/:course_id', (request, response) => {
