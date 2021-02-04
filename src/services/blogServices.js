0eec4f7452d9b6407941c02cf12746cd6255e3bf
AdarshNaidu <naidu.adarsh2511@gmail.com>
2021-02-04 22:56:20 +0530

    minor change

--- src/services/blogServices.js
+++ src/services/blogServices.js
@@ -1,20 +1,5 @@
 const Blog = require('../models/blog')
 
-const blogs = [
-	{
-		title: 'Async-Await',
-		body: 'Using async await results in cleaner code',
-	},
-	{
-		title: 'Handling Errors',
-		body: 'It\'s important to look for exceptions and handle them at the early stage',
-	},
-	{
-		title: 'Frequent Documentation',
-		body: 'Programs must be written for people to read, and only incidentally for machines to execute. - Harold Abelson',
-	},
-]
-
 const getAllBlogs = async () => {
 	const blogs = await Blog.find({})
 	return blogs
